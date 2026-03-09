a2d89063e2af6b4f0c5b8d39f5233974b6196831
Jabatar1992 <tundejabatar@gmail.com>
2026-03-09 16:12:32 +0100

    update

---
+++ api/user/auth/login.php
@@ -0,0 +1,36 @@
+<?php
+$method="POST";
+$cache="no-cache";
+include "../head.php";
+
+// user id and password
+if(isset($_POST['user_id']) && isset($_POST['password'])){
+    $user_id=cleanme($_POST['user_id']);
+    $password=cleanme($_POST['password']);
+    //validation
+    if(input_is_invalid($user_id) || input_is_invalid($password)){
+        respondBadRequest("User ID and password are required");
+    }else if(!is_numeric($user_id)){ 
+        respondBadRequest("User ID must be numeric");
+    }else {
+        $getdataemail =  $connect->prepare("SELECT * FROM user where id=? and password=?"); 
+        $getdataemail->bind_param("is",$user_id,$password);
+        $getdataemail->execute();
+        $getresultemail = $getdataemail->get_result();
+        if( $getresultemail->num_rows> 0){
+            $accesstoken=getTokenToSendAPI($user_id);
+          respondOK(["access_token"=>$accesstoken],"Login successful");
+        }else{ respondBadRequest(" user not found"); } 
+ }
+}else{
+   respondBadRequest("Invalid request. User ID and password are required.");
+} 
+ 
+
+
+
+
+
+
+
+?>
